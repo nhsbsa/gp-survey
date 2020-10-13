918d9e5a3f1a1c8912a7e4417eb97f0c9b24e544
Helen Spires <h.spiresnhs.net@unknown-f0:18:98:85:3d:32.lan>
2020-10-13 11:12:11 +0100

    all

--- app/routes.js
+++ app/routes.js
@@ -6,19 +6,34 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-
-    // The URL here needs to match the URL of the page that the user is on
-// when they type in their email address
 router.post('/email-address-page', function (req, res) {
 
     notify.sendEmail(
       // this long string is the template ID, copy it from the template
       // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
       // in your code.
-      '9f31952c-9848-4a77-b030-12cb43831703',
+      '2b2d0a1a-0f8c-4ae6-b84b-7f25f899e7f8',
       // `emailAddress` here needs to match the name of the form field in
       // your HTML page
       req.body.emailAddress
+    );
+  
+    // This is the URL the users will be redirected to once the email
+    // has been sent
+    res.redirect('/confirmation-page');
+  
+  });
+
+  router.post('/mobile-number-page', function (req, res) {
+
+    notify.sendSms(
+      // this long string is the template ID, copy it from the template
+      // page in GOV.UK Notify. It’s not a secret so it’s fine to put it
+      // in your code.
+      '01c1d3ad-845b-4e5a-99dc-16747ebc70fb',
+      // `emailAddress` here needs to match the name of the form field in
+      // your HTML page
+      req.body.mobileNumber
     );
   
     // This is the URL the users will be redirected to once the email
